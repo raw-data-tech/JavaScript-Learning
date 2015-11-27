e60a6926382056a5dbeddfcf8af91a3d2cb62f9d
Arun Ghosh <arunghosh@gmail.com>
2015-11-27 10:03:45 +0530

    formatted code

--- Class-Inheritance.js
+++ Class-Inheritance.js
@@ -8,79 +8,76 @@
 */
 
 
-
-
 //node module for getting input values from CLI
 var readline = require('readline');
 
 //initializing read-line object
 var rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
+    input: process.stdin,
+    output: process.stdout
 });
 
 //A new Class/prototyping in js for creating an object with a member function and a member variable 
-var Man = function (name) {
+var Man = function(name) {
 
-	//here this keyword is used to represent the 'this instance' of the class.
-	this.name = name;
-	this.sayHai = function(){
-		console.log('Hai I am '+this.name);
-	};
+    //here this keyword is used to represent the 'this instance' of the class.
+    this.name = name;
+    this.sayHai = function() {
+        console.log('Hai I am ' + this.name);
+    };
 };
 
 
 /*Another class by inheriting the first class and implemented function overloading
 you can see the sayHai function is overloaded here to implement some other datas to the out put.*/
-var Student = function(name,className){
-	/*this is how we inherit another class, in java we uses the 'extends' key word for this purpose.
-	here we sends 'this' object as first arguiment to the context of the parent class and then the 'Man' class will execute in same 
-	memory location of the sub class 'Student'.
-	*/
-	Man.call(this,name);
+var Student = function(name, className) {
+    /*this is how we inherit another class, in java we uses the 'extends' key word for this purpose.
+    here we sends 'this' object as first arguiment to the context of the parent class and then the 'Man' class will execute in same 
+    memory location of the sub class 'Student'.
+    */
+    Man.call(this, name);
 
-	//here we gives extra attributes that we need for the Student type objects
-	this.class = className;
-	this.sayHai = function(){
-		console.log('Hai I am '+this.name+' from '+this.class);	
-	};
+    //here we gives extra attributes that we need for the Student type objects
+    this.class = className;
+    this.sayHai = function() {
+        console.log('Hai I am ' + this.name + ' from ' + this.class);
+    };
 };
 
 
 rl.question("Are you a Student? (Y/N)", function(answer) {
-	newUser(answer,function(user){
-		user.sayHai();
-	});
+    newUser(answer, function(user) {
+        user.sayHai();
+    });
 });
 
 
-function newUser(answer,fn){
-	switch(answer){
-		case 'Y':
-		case 'y':
-			 // reading input from CLI by asking a question
-			rl.question("What is your name?", function(name) {
-				// reading another input from CLI after the user gives answer to the first question
-				rl.question("Enter your batch and department:", function(className) {
+function newUser(answer, fn) {
+    switch (answer) {
+        case 'Y':
+        case 'y':
+            // reading input from CLI by asking a question
+            rl.question("What is your name?", function(name) {
+                // reading another input from CLI after the user gives answer to the first question
+                rl.question("Enter your batch and department:", function(className) {
 
-				/*
-					We uses 'new' keyword for creating an object of a class.
-				*/
-				var User = new Student(name,className);
-				rl.close();
-				fn(User); // This is a call back function passed when calling newUser function.
-				});	
-			});
-			break;
-		case 'N':
-		case 'n':
-			rl.question("What is your name?", function(name) {
-				var User = new Man(name);
-				rl.close();
-				fn(User);
-			});
-			break;
-	}
+                    /*
+                    	We uses 'new' keyword for creating an object of a class.
+                    */
+                    var User = new Student(name, className);
+                    rl.close();
+                    fn(User); // This is a call back function passed when calling newUser function.
+                });
+            });
+            break;
+        case 'N':
+        case 'n':
+            rl.question("What is your name?", function(name) {
+                var User = new Man(name);
+                rl.close();
+                fn(User);
+            });
+            break;
+    }
 
 }
-
